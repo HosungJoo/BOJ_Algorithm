9aa44d0eaa0c40e00dc549e9fcbf89d0a482138e
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-02-15 16:56:45 +0900

    [Gold V] Title: 집합의 표현, Time: 44 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold V/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,60 @@
+#include<iostream>
+
+using namespace std;
+
+int parent[1000001];
+int Size[1000001];
+
+int find(int x) {
+	if (parent[x] == x) return x;
+	return parent[x] = find(parent[x]);
+}
+
+void Union(int x, int y) {
+	int px = find(x);
+	 int py = find(y);
+
+	if (px == py) return;
+	if (Size[px] < Size[py])
+		swap(px, py);
+	parent[py] = px;
+	Size[px] += Size[py];
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++) parent[i] = i;
+
+	for (int i = 0; i < m; i++) {
+		bool cal;
+		cin >> cal;
+
+		if (!cal) {
+			int a;
+			int b;
+			cin >> a >> b;
+			Union(a, b);
+		}
+		else {
+			int a;
+			int b;
+			cin >> a >> b;
+			if (find(a) == find(b)) {
+				cout << "YES" << "\n";
+			}
+			else {
+				cout << "NO" << "\n";
+			}
+		}
+	}
+
+
+	return 0;
+}
