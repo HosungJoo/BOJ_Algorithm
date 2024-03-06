9b5d08d95aa5ef577a27df40bbd85c0451a7b421
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-03-06 16:01:47 +0900

    [Silver III] Title: 동전 게임, Time: 24 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10837. 동전 게임/동전 게임.cc
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -7,32 +7,27 @@ int K, C;
 
 int main() {
 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     cin >> K >> C;
 
     for (int i = 0; i < C; i++) {
         int M, N;
         cin >> M >> N;
 
-        int dist = abs(M - N);
-
+        int dist = M - N;
         if (dist == 0) cout << 1 << "\n";
         else {
-            if (dist > K / 2 + 1) {
-                cout << 0 << "\n";
+            if (dist > 0) {
+                if (K - dist < M - 2) cout << 0 << "\n";
+                else cout << 1 << "\n";
             }
-            else if (dist == K / 2 + 1) {
-                if (K % 2 == 0) {
-                    if (N >= K / 2 + 1) cout << 0 << "\n";
-                    else cout << 1 << "\n";
-                }
-                else {
-                    if (N > K / 2 + 1) cout << 0 << "\n";
-                    else cout << 1 << "\n";
-                }
+            else if (dist < 0) {
+                if (K + dist < N - 1) cout << 0 << "\n";
+                else cout << 1 << "\n";
             }
-            else cout << 1 << "\n";
         }
-
         
     }
 
