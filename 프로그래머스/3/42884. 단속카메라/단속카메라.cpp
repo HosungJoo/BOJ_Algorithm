b85ef12a1c48b06ef8858d2e1ccea8fc2a022688
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-06-18 17:34:06 +0900

    Update 단속카메라.cpp

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -18,15 +18,15 @@ int solution(vector<vector<int>> routes) {
     for(int i=0;i<routes.size();i++){
         rt.push_back({routes[i][0],routes[i][1]});
     }
-    
+
+    //진입 시점 기준으로 sorting
     sort(rt.begin(),rt.end());
-    
     
     int cam = 0;
     location lo_cam = {-30001,-30001};
     
     for(int i=0;i<rt.size();i++){
-        
+        //카메라 설치 범위를 좁히다가 감당 못하면 카메라 새로 설치
         if(lo_cam.ec<rt[i].first){
             lo_cam.sc = rt[i].first;
             lo_cam.ec = rt[i].second;
