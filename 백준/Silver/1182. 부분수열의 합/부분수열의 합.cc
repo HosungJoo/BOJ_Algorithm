5354a11d3527efbed0a063d3d0fd66e36bb1bcff
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-07-27 14:20:36 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -6,15 +6,26 @@ int N, S;
 int nums[20];
 int ans;
 
+//처음 시도 44ms
 //한 칸씩 index를 밀어서 했던 조합 안하게 하기
-void dfs(int lv,int cnt,int idx,int sum) {
-	if (lv == cnt) {
-		if (sum == S) ans++;
+//void dfs(int lv,int cnt,int idx,int sum) {
+//	if (lv == cnt) {
+//		if (sum == S) ans++;
+//		return;
+//	}
+//	for (int i = idx; i < N; i++) {
+//		dfs(lv + 1, cnt, i + 1, sum + nums[i]);
+//	}
+//}
+
+//보고 배운 거 이게 더 좋은듯 4ms cnt는 원소 쓴 갯수 세기용
+void dfs(int lv,int sum,int cnt) {
+	if (lv == N) {
+		if (S == sum && cnt > 0) ans++;
 		return;
 	}
-	for (int i = idx; i < N; i++) {
-		dfs(lv + 1, cnt, i + 1, sum + nums[i]);
-	}
+	dfs(lv + 1, sum + nums[lv],cnt+1); //원소 넣기
+	dfs(lv + 1, sum, cnt); //원소 빼기
 }
 
 int main()
@@ -25,10 +36,7 @@ int main()
 		cin >> nums[i];
 	}
 	
-	//조합 갯수 i
-	for (int i = 1; i <= N; i++) {
-		dfs(0, i, 0, 0);
-	}
+	dfs(0, 0, 0);
 
 	cout << ans;
 
